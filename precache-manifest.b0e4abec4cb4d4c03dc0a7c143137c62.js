971a8290e289868e3b913bdf6056570f48314000
a-vrtn <vartanian.anooshik@gmail.com>
2019-01-21 02:04:07 +0400

    Updates

---
+++ precache-manifest.b0e4abec4cb4d4c03dc0a7c143137c62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f385fce96d0085a530e",
+    "url": "/static/js/main.6f385fce.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "6f385fce96d0085a530e",
+    "url": "/static/css/main.6771206b.chunk.css"
+  },
+  {
+    "revision": "8221fe0d18bef401e051dbd595239e13",
+    "url": "/index.html"
+  }
+];
